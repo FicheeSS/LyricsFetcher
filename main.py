113f295e702f7ac3a18ef040abc004a8c257dcc3
Fichee_SS <theo.ange.bocquet@gmail.com>
2019-05-15 16:35:38 +0200

    aled

--- main.py
+++ main.py
@@ -1,39 +1,42 @@
 import mutagen as mg
-import bs4 as bs
+from mutagen.flac import FLAC
+from bs4 import BeautifulSoup
+import urllib.request
 import glob
-import sys,getopt
+import sys
+SUPPORTEDFILES =(".flac",".ogg",".mp3")
+def url_contructor(artist,track):
+     # example : https://genius.com/Ariana-grande-7-rings-lyrics
+     url = "https://genius.com/" + artist[0].replace(" ","-").lower() + "-" + track[0].replace(" ","-").lower() + "-lyrics"
+     print(url)
+     return url
 
-def usage():
-    print("Usage : ")
-    print("       -i <path to the folder>")
-    print("       -h show this help")
-    print("       -v show verbose")
+def get_lyrics(artist,track):
+     html = ""
+     try :
+          req = urllib.request.Request(url_contructor(artist,track),headers={'User-Agent' : "Magic Browser"})
+          html = urllib.request.urlopen(req)
+     except urllib.error.HTTPError:
+          print("HTTP Error")
+          return ""
+     except urllib.error.URLError:
+          print("Incorect URL ")
+          return ""
+     
+     soup = BeautifulSoup(html,features="html.parser")
+     #print(soup.prettify())
+     soup = soup.find("div",attrs={"class" : "lyrics"})
+     soup = soup.get_text(separator=" ")
+     print(soup)
+     return soup
 
-def input():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:v", ["help","input="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(str(err))
-        usage()
-        sys.exit(2)
-    output = None
-    verbose = False
-    for o, a in opts:
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("-i","--input"):
-            input = a
-            print("input = " + input)
-            return input 
-        else:
-            assert False, "unhandled option"
 
-if len(input()) == 0:
-    print("input option empty")
-    sys.exit(2)
-else : 
-    
+fileList = []
+for ext in  (SUPPORTEDFILES):
+     fileList.append(glob.glob("**/*"+ext,recursive=True))
+#in case flac:
+for y in range (len(fileList[0])):
+     currentFile  = FLAC(fileList[0][y])
+     print(str(currentFile))
+     currentFile["UNSYNCEDLYRICS"] = get_lyrics(currentFile["Artist"],currentFile["title"])
+     currentFile.save()
