44a1860eeceaa63c053f68c37ca28db6f760cf89
FicheeSS <theo.ange.bocquet@gmail.com>
2019-07-02 17:01:36 +0200

    FicheeSS

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from mutagen.mp3 import MP3
 from mutagen.oggvorbis import OggVorbis
 import glob
 import sys
+import re
 SUPPORTEDFILES =(".flac",".ogg",".mp3")
 LYRICSTAGS = ("UNSYNCEDLYRICS","LYRICS")
 def url_contructor(artist,track):
      # example : https://genius.com/Ariana-grande-7-rings-lyrics
-     url = "https://genius.com/" + artist[0].replace(" ","-").lower() + "-" + track[0].replace(" ","-").lower() + "-lyrics"
+     regex = re.compile(".*?\((.*?)\)")
+     track[0] = re.findall(regex, track[0])
+     print(track[0])
+     url = "https://genius.com/" + artist[0].replace(" ","-").lower() + "-" + str(track[0]).replace(" ","-").replace("ä","a").lower() + "-lyrics"
      #print(url)
      return url
 
@@ -25,13 +29,14 @@ def get_lyrics(artist,track):
      except urllib.error.URLError:
           print("Incorect URL for "+str(url_contructor(artist,track)) )
           return ""
-     
+     except UnicodeEncodeError :
+          print("Non-Ascii symbol in "+ str(artist) + " "+ str(track))
+          return ""
      soup = BeautifulSoup(html,features="html.parser")
      #print(soup.prettify())
      soup = soup.find("div",attrs={"class" : "lyrics"})
      soup = soup.get_text(separator=" ")
      #print(soup)
-     print("Lyrics find for " + str(artist) + " " + str(track))
      return soup
 
 
@@ -41,11 +46,12 @@ for ext in  (SUPPORTEDFILES):
 #in case flac:
 for y in range (len(fileList[0])):
      currentFile  = FLAC(fileList[0][y])
-     #print(str(currentFile))
      lyrics = get_lyrics(currentFile["Artist"],currentFile["title"])
-     for LyricsTags in LYRICSTAGS:
-          currentFile[LyricsTags] = lyrics
-     currentFile.save()
+     if len(lyrics) != 0: 
+          print("File " + str(y) +" / " + str(len(fileList[0]))+  " : " + str(int(y/len(fileList[0])*100)) + " %")
+          for LyricsTags in LYRICSTAGS:
+               currentFile[LyricsTags] = lyrics
+          currentFile.save()
 for y in range (len(fileList[1])):
      currentFile = OggVorbis(fileList[1][y])
      lyrics = get_lyrics(currentFile["Artist"],currentFile["title"])
