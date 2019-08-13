67963b810fb946a8b1ffbb79a17fa6175f0a979d
FicheeSS <theo.ange.bocquet@gmail.com>
2019-08-13 12:53:20 +0200

    working as intended

---
+++ Options.py
@@ -0,0 +1,4 @@
+SUPPORTEDFILES =(".flac",".ogg",".mp3",".m4a",".mp4")
+LYRICSTAGS = ("UNSYNCEDLYRICS","LYRICS")
+URL_CARSET = [("ä","a"),(" ","-"),("'",""),(",",""),("/","-"),(".",""),("û","u"),("ü","u"),("ù","u"),("?",""),("&","and"),(":","")]
+JobLimit = 20
