3ce39ac48a8aa2f7c0e7cd908529de222d68ccbc
FicheeSS <theo.ange.bocquet@gmail.com>
2019-08-13 12:53:20 +0200

    working as intended

--- main.py
+++ main.py
@@ -1,61 +1,26 @@
 import mutagen as mg
 from mutagen.flac import FLAC
-from bs4 import BeautifulSoup
-import urllib.request
 from mutagen.mp3 import MP3
 from mutagen.oggvorbis import OggVorbis
 from mutagen.easymp4 import EasyMP4
+from mutagen.easyid3 import EasyID3 
 import glob
 import sys
 import multiprocessing
-import re
-from mutagen.easyid3 import EasyID3
+from FilesTools import SearchMusicFiles
+import time
+from LyricsTools import GetLyrics 
+from Options import JobLimit , SUPPORTEDFILES ,  LYRICSTAGS
+import os
 
-SUPPORTEDFILES =(".flac",".ogg",".mp3",".m4a",".mp4")
-LYRICSTAGS = ("UNSYNCEDLYRICS","LYRICS")
 TOTALFILES = 0
-URL_CARSET = [("ä","a"),(" ","-"),("'",""),(",",""),("/","-"),(".",""),("û","u"),("ü","u"),("ù","u"),("?","")]
-JobLimit = 50
-
-def url_contructor(artist,track):
-     regexparen = re.compile(".*?\((.*?)\)")
-     regexcrochet = re.compile(".*?\[.*?\]")
-     for delete in re.findall(regexparen, track[0]):
-          track[0] = str(track[0]).replace(str(delete),"").replace("(","").replace(")","")
-     for delete in re.findall(regexcrochet, track[0]):
-          track[0] = str(track[0]).replace(str(delete),"").replace("[","").replace("]","")
-     track[0] = str(track[0]).rstrip()
-     for carset in URL_CARSET : 
-          track[0] = track[0].replace(carset[0],carset[1])
-          artist[0] = artist[0].replace(carset[0],carset[1])
-     url = "https://genius.com/" + artist[0].lower() + "-" + track[0].lower()+ "-lyrics"
-     return url
-
-def get_lyrics(artist,track):
-     html = ""
-     try :
-          req = urllib.request.Request(url_contructor(artist,track),headers={'User-Agent' : "Magic Browser"})
-          html = urllib.request.urlopen(req)
-     except urllib.error.HTTPError:
-          print("Lyrics not found " + str(artist) + " "+ str(track))
-          return ""
-     except urllib.error.URLError:
-          print("Incorect URL for "+str(url_contructor(artist,track)) )
-          return ""
-     except UnicodeEncodeError :
-          print("Non-Ascii symbol in "+ str(artist) + " "+ str(track))
-          return ""
-     soup = BeautifulSoup(html,features="html.parser")
-     soup = soup.find("div",attrs={"class" : "lyrics"})
-     soup = soup.get_text(separator=" ")
-     return soup
 
 def GetLyricsFromFile(MFile):  
      try :
-          lyrics = get_lyrics(MFile["artist"],MFile["title"]) 
+          lyrics = GetLyrics(MFile["artist"],MFile["title"]) 
      except KeyError:
           try :
-               lyrics = get_lyrics(MFile["Artist"],MFile["Title"])
+               lyrics = GetLyrics(MFile["Artist"],MFile["Title"])
           except KeyError:
                print("File is lacking at least one tags ")
                return ""
@@ -94,36 +59,11 @@ def SetLyricsToFiles(MFile):
 
 if __name__ == '__main__':
      #q = Queue.Queue("50")
-     fileList = []
-     ProcessFile = []
-     for ext in  (SUPPORTEDFILES):
-          fileList.append(glob.glob("**/*"+ext,recursive=True))
-     for SFiles in SUPPORTEDFILES:
-          if SFiles == ".flac":
-               for y in range(len(fileList[0])):
-                    ProcessFile.append(FLAC(fileList[0][y]))
-                    TOTALFILES += 1
-          elif SFiles == ".ogg":
-               for y in range(len(fileList[1])):
-                    ProcessFile.append(OggVorbis(fileList[1][y]))
-                    TOTALFILES += 1
-          elif SFiles == ".mp3":
-               for y in range(len(fileList[2])):
-                    try :
-                         ProcessFile.append(EasyID3(fileList[2][y]))
-                         TOTALFILES += 1     
-                    except :
-                         ProcessFile.append(MP3(fileList[2][y]))
-                         TOTALFILES += 1
-          elif SFiles == ".m4a" or SFiles == ".mp4":
-               for y in range(len(fileList[3])):
-                    ProcessFile.append(EasyMP4(fileList[3][y]))
-                    TOTALFILES += 1
-               for y in range(len(fileList[4])):
-                    ProcessFile.append(EasyMP4(fileList[4][y]))
-                    TOTALFILES += 1
-
-     print(str(TOTALFILES) + " files found")
+     ProcessFile = SearchMusicFiles(os.getcwd())
+     print(ProcessFile)
+     startupTime = time.time()
+     print(str(len(ProcessFile)) + " files found")
+     #Cut the ProcessFile list into list of JobLimit size
      JobsFile = []
      for i in range(int(len(ProcessFile)/JobLimit+1)):
           JobsFile.append([])
@@ -134,15 +74,17 @@ if __name__ == '__main__':
           else :
                for y in range(len(ProcessFile) - JobLimit * i) :
                     JobsFile[i].append(ProcessFile[(i+1) * y])
+     
      jobs = []
      for i in range(len(JobsFile)) : 
           for y in range(len(JobsFile[i])):
                p = multiprocessing.Process(target=SetLyricsToFiles, args=(JobsFile[i][y],))
-               jobs.append(p)
                p.start()
+               jobs.append(p)
           for proc in jobs :
                proc.join()
           print("Batch : " + str(i+1) + " out of : " + str(len(JobsFile)))
-
+     
+     print("Time taken : " + str(time.time() - startupTime))
      print("All files have been processed ")
 
