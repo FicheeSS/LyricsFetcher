50c67657389359c51c6ead4d6d4d7aead0f8b7e8
FicheeSS <theo.ange.bocquet@gmail.com>
2019-08-29 21:43:17 +0200

    first working version

---
+++ apitest.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from LyricsTools import GetLyrics
+
+app = Flask(__name__)
+api = Api(app)
+
+class Lyrics (Resource):
+    def get(self, artist , track):
+        artist = "sirenia"
+        track = "decadence"
+        print("artist = "+ artist + " track = " + track )
+        if artist == "" or track == "":
+            return "One of the input is empty" , 400
+        else :
+            lyrics = GetLyrics(artist,track)
+            if lyrics == "" :
+                return "Could not find the requested lyrics" , 204
+            else: 
+                return  lyrics , 200 
+
+api.add_resource(
+    Lyrics,
+    '/lyrics/<artist>/<track>', 
+    methods=["GET"]
+)
+app.run(debug=True)
