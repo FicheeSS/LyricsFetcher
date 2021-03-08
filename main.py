86c39ef409c3ea3ac71273574af9e3ec33ad3eb9
FicheeSS <theo.ange.bocquet@gmail.com>
2021-03-08 11:28:59 +0100

    all file in one and added cli support + requirements

--- main.py
+++ main.py
@@ -7,13 +7,82 @@ from mutagen.easyid3 import EasyID3
 import glob
 import sys
 import multiprocessing
-from FilesTools import SearchMusicFiles
 import time
-from LyricsTools import GetLyrics 
-from Options import JobLimit , SUPPORTEDFILES ,  LYRICSTAGS
 import os
+from bs4 import BeautifulSoup
+import urllib.request
+import re
+import argparse
+
+SUPPORTEDFILES =(".flac",".ogg",".mp3",".m4a",".mp4")
+LYRICSTAGS = ("UNSYNCEDLYRICS","LYRICS")
+URL_CARSET = [("ä","a"),(" ","-"),("'",""),(",",""),("/","-"),(".",""),("û","u"),("ü","u"),("ù","u"),("?",""),("&","and"),(":","")]
+JobLimit = 20
+
 
 TOTALFILES = 0
+def UrlConstructor(artist,track):
+     regexparen = re.compile(".*?\((.*?)\)")
+     regexcrochet = re.compile(".*?\[.*?\]")
+     for delete in re.findall(regexparen, track[0]):
+          track[0] = str(track[0]).replace(str(delete),"").replace("(","").replace(")","")
+     for delete in re.findall(regexcrochet, track[0]):
+          track[0] = str(track[0]).replace(str(delete),"").replace("[","").replace("]","")
+     track[0] = str(track[0]).rstrip()
+     for carset in URL_CARSET : 
+          track[0] = track[0].replace(carset[0],carset[1])
+          artist[0] = artist[0].replace(carset[0],carset[1])
+     url = "https://genius.com/" + artist[0].lower() + "-" + track[0].lower()+ "-lyrics"
+     return url
+
+def GetLyrics(artist,track):
+     html = ""
+     try :
+          url = UrlConstructor(artist,track)
+          if not url:
+               return ""
+          req = urllib.request.Request(url,headers={'User-Agent' : "Magic Browser"})
+          html = urllib.request.urlopen(req)
+     except urllib.error.HTTPError:
+          print("Lyrics not found " + str(artist) + " "+ str(track))
+          return ""
+     except urllib.error.URLError:
+          print("Incorect URL for "+str(UrlConstructor(artist,track)) )
+          return ""
+     except UnicodeEncodeError :
+          print("Non-Ascii symbol in "+ str(artist) + " "+ str(track))
+          return ""
+     try:
+          soup = BeautifulSoup(html,features="html.parser").find("div",attrs={"class" : "lyrics"}).get_text(separator=" ")
+     except :
+          soup =""
+     finally:
+          return soup
+
+def SearchMusicFiles(dir) : 
+    fileList = []
+    ProcessFile = []
+    for ext in  (SUPPORTEDFILES):
+        fileList.append(glob.glob((dir+"/**/*"+ext),recursive=True))
+    for SFiles in SUPPORTEDFILES:
+        if SFiles == ".flac":
+            for y in range(len(fileList[0])):
+                ProcessFile.append(FLAC(fileList[0][y]))
+        elif SFiles == ".ogg":
+            for y in range(len(fileList[1])):
+                ProcessFile.append(OggVorbis(fileList[1][y]))
+        elif SFiles == ".mp3":
+            for y in range(len(fileList[2])):
+                try :
+                        ProcessFile.append(EasyID3(fileList[2][y]))  
+                except :
+                        ProcessFile.append(MP3(fileList[2][y]))
+        elif SFiles == ".m4a" or SFiles == ".mp4":
+            for y in range(len(fileList[3])):
+                ProcessFile.append(EasyMP4(fileList[3][y]))
+            for y in range(len(fileList[4])):
+                ProcessFile.append(EasyMP4(fileList[4][y]))
+    return ProcessFile
 
 def GetLyricsFromFile(MFile):  
      
@@ -53,15 +122,22 @@ def SetLyricsToFiles(MFile):
                               MFile["lyrics"] = lyrics
                               MFile.save()
                          except :
-                              print("Please use modified Mutagen EasyMp4 library with lyrics tag")
-#     else :
+                              print("Please use modified Mutagen EasyMp4 library with lyrics tag")#     else :
           #Nfailedfiles += 1
       
 
 if __name__ == '__main__':
-     #q = Queue.Queue("50")
-     ProcessFile = SearchMusicFiles(os.getcwd())
-     print(ProcessFile)
+     parser = argparse.ArgumentParser()
+     parser.add_argument(dest='loc', metavar='d', type=str,
+                         help='process file location ')
+     args = parser.parse_args()
+     if not args.loc or not os.path.exists(args.loc):
+          print("Using current working directory")
+          ProcessFile = SearchMusicFiles(os.getcwd())
+     else :
+          print("Using provided directory")
+          ProcessFile = SearchMusicFiles(args.loc)
+     #print(ProcessFile)
      startupTime = time.time()
      print(str(len(ProcessFile)) + " files found")
      #Cut the ProcessFile list into list of JobLimit size
@@ -75,7 +151,7 @@ if __name__ == '__main__':
           else :
                for y in range(len(ProcessFile) - JobLimit * i) :
                     JobsFile[i].append(ProcessFile[(i+1) * y])
-     
+     print("Starting to process files ")
      jobs = []
      for i in range(len(JobsFile)) : 
           for y in range(len(JobsFile[i])):
@@ -88,4 +164,5 @@ if __name__ == '__main__':
      
      print("Time taken : " + str(time.time() - startupTime))
      print("All files have been processed ")
+     sys.exit(0)
 
